4c9425914dc07a82a2b6681a35e5f00893b75dda
Foster <72973015+Foster0123@users.noreply.github.com>
2023-01-09 12:48:47 +0530

    Initial Commit 2

--- src/components/Drum-Play-Buttons.jsx
+++ src/components/Drum-Play-Buttons.jsx
@@ -1,83 +1,73 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Button from 'react-bootstrap/Button'
 
 const DrumPlayButtons = () => {
-	const playButtons = [
-	{
-		id:0,
-		audioSrc:""
-	},
-	{
-		id:1,
-		audioSrc:""
-	},
-	{
-		id:2,
-		audioSrc:""
-	},
-	{
-		id:3,
-		audioSrc:""
-	},
-	{
-		id:4,
-		audioSrc:""
-	},
-	{
-		id:5,
-		audioSrc:""
-	},
-	{
-		id:6,
-		audioSrc:""
-	},
-	{
-		id:7,
-		audioSrc:""
-	},
-	{
-		id:8,
-		audioSrc:""
-	},
+	useEffect(() => {
+		document.addEventListener('keydown', (e) => {
+			switch (e.key) {
+				case 'q':
+
+			}
+		})
+	}, [])
+	const buttons = [
+		{
+			id: 'q',
+			audioSrc: '',
+			name: ''
+		},
+		{
+			id: 'w',
+			audioSrc: '',
+			name: ''
+		},
+		{
+			id: 'e',
+			audioSrc: '',
+			name: ''
+		},
+		{
+			id: 'a',
+			audioSrc: '',
+			name: ''
+		},
+		{
+			id: 's',
+			audioSrc: '',
+			name: ''
+		},
+		{
+			id: 'd',
+			audioSrc: '',
+			name: ''
+		},
+		{
+			id: 'z',
+			audioSrc: '',
+			name: ''
+		},
+		{
+			id: 'x',
+			audioSrc: '',
+			name: ''
+		},
+		{
+			id: 'c',
+			audioSrc: '',
+			name: ''
+		},
 	]
 	return (
 		<div className="play-button-container">
 			<div className="drum-play-buttons">
-				<Button variant="primary" className='drum-pad'>Q
-					<audio src=""  />
-				</Button>
-
-				<Button variant="primary" className='drum-pad'>W
-					<audio src="" />
-				</Button>
-
-				<Button variant="primary" className='drum-pad'>E
-					<audio src="" />
-				</Button>
-
-				<Button variant="primary" className='drum-pad'>A
-					<audio src="" />
-				</Button>
-
-				<Button variant="primary" className='drum-pad'>S
-					<audio src="" />
-				</Button>
-
-				<Button variant="primary" className='drum-pad'>D
-					<audio src="" />
-				</Button>
-
-				<Button variant="primary" className='drum-pad'>Z
-					<audio src="" />
-				</Button>
-
-				<Button variant="primary" className='drum-pad'>X
-					<audio src="" />
-				</Button>
-
-				<Button variant="primary" className='drum-pad'>C
-					<audio src="" />
-				</Button>
+				{buttons.map((btn, index) => {
+					return (
+						<Button variant="none" id={btn.id} key={index} className='drum-pad text-uppercase'>
+							{btn.id}
+							<audio src={btn.audioSrc} />
+						</Button>
+					)
+				})}
 			</div>
 		</div>
 	)
